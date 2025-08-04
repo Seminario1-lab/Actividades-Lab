08073be98e3a0667b789423869670d4531eefdac
82 Gerber Ordoñez <gemersonordonez@gmail.com>
2025-08-04 16:53:09 -0600

    Clase 3 RDS y Balanceador

---
+++ LAB-SEMI1A-2S2025/Clase3/Clase3RDS/credenciales.js
@@ -0,0 +1,7 @@
+module.exports = {
+    host: '',        // Cambia esto a la dirección de tu base de datos
+    user: '',             // Tu usuario de la base de datos
+    password: '',     // La contraseña de tu base de datos
+    database: '', // El nombre de la base de datos que usarás
+
+};
